b55275fc3d3471a895251cbf293dc422a801dce2
Jose Flores <66032729+Josefloresh94@users.noreply.github.com>
2024-07-20 20:58:08 -0600

    Paginacion

--- src/main/java/com/springboot/backend/jose/usersapp/users_backend/services/UserService.java
+++ src/main/java/com/springboot/backend/jose/usersapp/users_backend/services/UserService.java
@@ -3,6 +3,9 @@ package com.springboot.backend.jose.usersapp.users_backend.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.springboot.backend.jose.usersapp.users_backend.entities.User;
 
 import io.micrometer.common.lang.NonNull;
@@ -10,6 +13,8 @@ import io.micrometer.common.lang.NonNull;
 public interface UserService {
 
     List<User> findAll();
+    
+    Page<User> findAll(Pageable pageable);
 
     Optional<User> findById(@NonNull Long id);
 
