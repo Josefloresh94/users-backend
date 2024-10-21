fa55d0793e7cd72fde56a61f9cfcedc5fb36f5a6
Jose Flores <66032729+Josefloresh94@users.noreply.github.com>
2024-10-20 22:20:18 -0600

    todo

--- src/main/java/com/springboot/backend/jose/usersapp/users_backend/entities/User.java
+++ src/main/java/com/springboot/backend/jose/usersapp/users_backend/entities/User.java
@@ -1,10 +1,19 @@
 package com.springboot.backend.jose.usersapp.users_backend.entities;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -39,5 +48,15 @@ public class User {
 
     @NotBlank
     private String password;
+
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "users_roles",
+        joinColumns = {@JoinColumn(name="user_id")},
+        inverseJoinColumns = {@JoinColumn(name="role_id")},
+        uniqueConstraints = { @UniqueConstraint(columnNames = {"user_id", "role_id"})}
+    )
+    private List<Role> roles;
 
 }
